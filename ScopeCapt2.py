e35d7a9d1838d85f87a18453333fe14b72e8484f
KevinWang34 <26266974+kw81634dr@users.noreply.github.com>
2021-03-08 18:09:35 -0800

    filename need fix

--- ScopeCapt2.py
+++ ScopeCapt2.py
@@ -1,5 +1,5 @@
-from datetime import datetime # std library
-import pyvisa as visa # https://pyvisa.readthedocs.org/en/stable/
+from datetime import datetime  # std library
+import pyvisa as visa  # https://pyvisa.readthedocs.org/en/stable/
 import os
 from pathlib import Path
 
@@ -9,7 +9,8 @@ import numpy as np
 import cv2
 
 import tkinter as tk
-from tkinter import ttk, Entry, filedialog
+from tkinter import ttk, Entry, filedialog, IntVar
+
 
 # https://pythonguides.com/python-tkinter-menu-bar/
 # https://coderslegacy.com/python/list-of-tkinter-widgets/
@@ -54,14 +55,21 @@ class App:
 
         self.frame = tk.Frame(self.master)
 
-        #self.master.geometry("+%d+%d" % (self.frame.window_start_x, self.frame.window_start_y))
+        # self.master.geometry("+%d+%d" % (self.frame.window_start_x, self.frame.window_start_y))
         self.master.title("KW Scope Capture")
 
         self.target_gpib_address = tk.StringVar()
         self.status_var = tk.StringVar()
         self.path_var = tk.StringVar()
+
         self.filename_var = tk.StringVar()
+        self.filename_var.set('DSO')
+
         self.IDN_of_scope = tk.StringVar()
+        self.imshow_var = IntVar()
+        self.continuous_var = IntVar()
+        self.add_timestamp_var = IntVar()
+        self.add_timestamp_var.set(1)
 
         # self.GPIB_list = ['GPIB::6::INSTR']
 
@@ -71,11 +79,10 @@ class App:
         self.IDN_of_scope.set('')
 
         self.dt = datetime.now()
-        self.visa_timeout_duration = 5000    # in ms
+        self.visa_timeout_duration = 5000  # in ms
 
         colum = 3
         row = 4
-
 
         # self.frame.columnconfigure(0, pad=3)
         # self.frame.columnconfigure(1, pad=3)
@@ -89,13 +96,13 @@ class App:
         # self.frame.rowconfigure(4, pad=3)
 
         # row 0
-        #label_GPIB_address = tk.Label(self.frame, text="GPIB Address")
-        #label_GPIB_address.grid(row=0, column=0)
-        #btn_scan_gpib = tk.Button(self.frame, text="Scan Scope", command=self.scan_gpib)
-        #btn_scan_gpib.grid(row=0, column=0)
-        #Combo = ttk.Combobox(self.frame, values=self.GPIB_list, width=35)
-        #Combo.set("Pick an Option")
-        #Combo.grid(row=0, column=1)
+        # label_GPIB_address = tk.Label(self.frame, text="GPIB Address")
+        # label_GPIB_address.grid(row=0, column=0)
+        # btn_scan_gpib = tk.Button(self.frame, text="Scan Scope", command=self.scan_gpib)
+        # btn_scan_gpib.grid(row=0, column=0)
+        # Combo = ttk.Combobox(self.frame, values=self.GPIB_list, width=35)
+        # Combo.set("Pick an Option")
+        # Combo.grid(row=0, column=1)
         # btn_connect_scope = tk.Button(self.frame, text="Get Scope info", command=self.get_scope_info)
         # btn_connect_scope.grid(row=0, column=2)
 
@@ -103,26 +110,40 @@ class App:
         label_entry_dir = tk.Label(self.frame, text="Save to Folder")
         label_entry_dir.grid(row=1, column=0)
         self.E_dir = tk.Entry(self.frame, textvariable=self.path_var, width=37)
-        self.E_dir.grid(row=1, column=1)
+        self.E_dir.grid(row=1, column=1, columnspan=2)
         btn_prompt_dir = tk.Button(self.frame, text="Prompt(P)", command=self.prompt_path)
-        btn_prompt_dir.grid(row=1, column=2)
+        btn_prompt_dir.grid(row=1, column=3)
         self.frame.bind('p', lambda event: self.prompt_path())
 
         # row 2
         label_entry_filename = tk.Label(self.frame, text="File Name")
         label_entry_filename.grid(row=2, column=0)
         self.E_filename = tk.Entry(self.frame, textvariable=self.filename_var, width=37)
-        self.E_filename.grid(row=2, column=1)
-        btn_use_time = tk.Button(self.frame, text="Time Stamp", command=self.get_default_filename)
-        btn_use_time.grid(row=2, column=2)
+        self.E_filename.grid(row=2, column=1, columnspan=2)
+        # btn_use_time = tk.Button(self.frame, text="Add TimeStamp", command=self.get_default_filename)
+        # btn_use_time.grid(row=2, column=2)
+        chkbox_addTimeStamp = tk.Checkbutton(self.frame, text='Add Time Stamp', variable=self.add_timestamp_var,
+                                             onvalue=1, offvalue=0,
+                                             command=None)
+        chkbox_addTimeStamp.grid(row=2, column=3)
 
         # row 3
         btn_capture = tk.Button(self.frame, text="Capture (Enter)", command=self.btn_capture_clicked)
-        btn_capture.grid(row=3, column=1)
-        self.frame.bind('<Return>', lambda event: self.get_shot_scope(self.path_var.get()))
-        #label.grid(row=2, column=0, padx=1, pady=1, ipady=3)
+        btn_capture.grid(row=3, column=0)
+        btn_capture.bind('c', lambda event: self.get_shot_scope())
+        # label.grid(row=2, column=0, padx=1, pady=1, ipady=3)
+
+        chkbox_imshow = tk.Checkbutton(self.frame, text='ShowImage', variable=self.imshow_var, onvalue=1, offvalue=0,
+                                       command=None)
+        chkbox_imshow.grid(row=3, column=1)
+
+        chkbox_Continuous = tk.Checkbutton(self.frame, text='Continuous Mode', variable=self.continuous_var, onvalue=1,
+                                           offvalue=0,
+                                           command=None)
+        chkbox_Continuous.grid(row=3, column=2)
+
         btn_exit = tk.Button(self.frame, text="Exit", command=self.client_exit)
-        btn_exit.grid(row=3, column=2)
+        btn_exit.grid(row=3, column=3)
 
         # status bar
         status_bar = tk.Label(self.frame, textvariable=self.status_var, bd=1, relief=tk.SUNKEN, anchor=tk.W)
@@ -161,13 +182,14 @@ class App:
         time_now = self.dt.strftime("%H%M%S.png")
         # time_now = self.dt.strftime("_%Y%m%d_%H%M%S.png")
         print("Will Save Image to ", time_now)
-        get_exist_text = self.filename_var.get()
-        if get_exist_text != '':
-            default_filename = get_exist_text + '_' + time_now
+        # get_exist_text = self.filename_var.get()
+
+        if self.add_timestamp_var:
+            default_filename = self.filename_var.get() + '_' + time_now
         else:
-            default_filename = 'scope_' + time_now
+            default_filename = 'DSO'
         self.filename_var.set(default_filename)
-        self.status_var.set("Time Stamp Applied")
+        # self.status_var.set("Time Stamp Applied")
 
     # def start_n_stop_scope_accquisition(self):
     #     # try:
@@ -198,26 +220,32 @@ class App:
 
                 save_path = Path(self.path_var.get()) / Path(self.filename_var.get())
                 print("SAVE PATH:", save_path)
-                with open(save_path, "wb") as imgFile:
-                    imgFile.write(img_data)
-                    imgFile.close()
-                    print("Saved!")
-                    self.status_var.set(save_path)
+
+                try:
+                    with open(save_path, "wb") as imgFile:
+                        imgFile.write(img_data)
+                        imgFile.close()
+                        print("Saved!")
+                        self.status_var.set(save_path)
+                except IOError:
+                    self.status_var.set("Cannot save file, check folder and filename")
 
                 scope.close()
                 rm.close()
 
-                file_png_data = BytesIO(img_data)
-                dt = Image.open(file_png_data)
-                I = np.asarray(dt)
-                print("Got image, shape:", I.shape)
-                I_cv2 = cv2.cvtColor(I, cv2.COLOR_RGB2BGR)
-                cv2.imshow("Shot", I_cv2)
-                cv2.waitKey()
-                cv2.destroyAllWindows()
+                if self.imshow_var.get():
+                    file_png_data = BytesIO(img_data)
+                    dt = Image.open(file_png_data)
+                    I = np.asarray(dt)
+                    print("Got image, shape:", I.shape)
+                    I_cv2 = cv2.cvtColor(I, cv2.COLOR_RGB2BGR)
+                    cv2.imshow("Captured, press any key to dismiss", I_cv2)
+                    cv2.waitKey()
+                    cv2.destroyAllWindows()
 
         except ValueError:
             self.status_var.set("CANNOT connect to Scope")
+        self.get_default_filename()
 
     def prompt_path(self):
         folder_prompted = filedialog.askdirectory()
